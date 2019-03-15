d730a0dd031fa2af612a9174364a3ee164facf49
Justin Wong <justin@cosmic.org.uk>
2019-03-15 16:50:42 +0000

    Add symbol URL builder

--- src/ExIconsManagerInterface.php
+++ src/ExIconsManagerInterface.php
@@ -8,6 +8,21 @@ namespace Drupal\ex_icons;
 interface ExIconsManagerInterface {
 
   /**
+   * Builds the relative URL for use in <use> SVG elements.
+   *
+   * The URL will have a query cache buster generated from the plugin
+   * information discovered.
+   *
+   * @param string $plugin_id
+   *   The plugin_id.
+   *
+   * @return string
+   *   The URL. For example,
+   *   '/modules/example/dist/icons.svg?63d2e2bf1452aee5#icon'.
+   */
+  public function buildUrl($plugin_id);
+
+  /**
    * Returns the inline defs markup.
    *
    * @return string[]
